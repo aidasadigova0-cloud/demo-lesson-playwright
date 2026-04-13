7c7cc077f9091a315c61fa31a895f4e39f1a2500
Aida Sadigova <aida.sadigova@Aidas-MacBook.local>
2026-04-14 00:29:58 +0300

    fix tests

--- tests/ui/e2e-flow-simple.spec.ts
+++ tests/ui/e2e-flow-simple.spec.ts
@@ -3,20 +3,29 @@ import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
-test('signIn button disabled when incorrect data inserted', async ({ page }) => {
-  const loginPage = new LoginPage(page)
+let loginPage: LoginPage
+
+test.beforeEach(async ({ page }) => {
+  loginPage = new LoginPage(page)
   await loginPage.open()
+})
+
+test('signIn button disabled when incorrect data inserted', async ({}) => {
   await loginPage.usernameField.fill(faker.lorem.word(2))
   await loginPage.passwordField.fill(faker.lorem.word(7))
   await expect(loginPage.signInButton).toBeDisabled()
 })
 
+test('error message displayed when incorrect credentials used', async ({}) => {
+  // implement test
+})
 
-test('login with correct credentials and verify order creation page', async ({ page }) => {
-  const loginPage = new LoginPage(page)
-  await loginPage.open()
+test('login with correct credentials and verify order creation page', async ({}) => {
   const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
-  await orderCreationPage.statusButton.click({ force: true })
-
+  await expect(orderCreationPage.statusButton).toBeVisible()
   // verify at least few elements on the order creation page
+})
+
+test('login and create order', async ({}) => {
+  // implement test
 })
